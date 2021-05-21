9e676f261c369136b1c35fd5edd1f1d651047ff9
harrydehix <harry.holly02@gmail.com>
2021-05-21 18:14:53 +0200

    Update URLs

--- index.js
+++ index.js
@@ -91,5 +91,7 @@ io.use(function (socket, next) {
 });
 
 server.listen(3000, () => {
-    console.log("Listening on localhost:3000...");
+    console.log(
+        "Listening on https://simple-socket-io-jwt-chat-app.herokuapp.com/:3000..."
+    );
 });
