6a6bb153232740c412aa00985d93f1ef42ff7d2e
harrydehix <harry.holly02@gmail.com>
2021-05-22 11:55:00 +0200

    Divide html and js on client side

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const globalErrorHandler = require("./controllers/errorController");
 // MIDDLEWARE
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static(`${__dirname}/assets`));
 app.use("/api/v1/auth", require("./routers/authRouter"));
 app.use(globalErrorHandler);
 
-// HOMEPAGE
-app.get("/", (req, res) => {
-    res.sendFile(`${__dirname}/assets/index.html`);
-});
+// // HOMEPAGE
+// app.get("/", (req, res) => {
+//     res.sendFile(`${__dirname}/assets/index.html`);
+// });
 
 module.exports = app;
---
+++ assets/client.js
@@ -0,0 +1,78 @@
+(async () => {
+    var token;
+    var you;
+    const res = await Swal.fire({
+        title: "Whats your name?",
+        input: "text",
+        text: "Enter your name to start chatting...",
+        icon: "question",
+        allowOutsideClick: false,
+        confirmButtonText: "Enter",
+        inputValidator: async (value) => {
+            try {
+                const res = await axios.post("/api/v1/auth/login", {
+                    username: value,
+                });
+                token = res.data.token;
+                you = value;
+            } catch (err) {
+                if (err.response) return err.response.data.message;
+                return err.message;
+            }
+        },
+    });
+    console.log("Got token:", token);
+    const socket = io(undefined, {
+        query: { token },
+    });
+
+    socket.once("connect", () => {
+        console.log("Connected to server!");
+        writeToMessageList(`<i>You joined the chat!<i/>`, true);
+        socket.on("chat message", function (data) {
+            writeMessage(data.username, data.msg);
+        });
+        socket.on("user joined", function (username) {
+            writeToMessageList(`<i>${username} joined</i>`, true);
+        });
+        socket.on("user left", function (username) {
+            writeToMessageList(`<i>${username} left</i>`, true);
+        });
+    });
+
+    const message = {
+        list: document.getElementById("messages"),
+        form: document.getElementById("form"),
+        input: document.getElementById("input"),
+    };
+
+    message.form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        if (message.input.value) {
+            socket.emit("chat message", message.input.value);
+            writeMessage(`You`, message.input.value);
+            message.input.value = "";
+        }
+    });
+
+    function writeToMessageList(content, allowFormatting) {
+        var item = document.createElement("li");
+        if (allowFormatting) item.innerHTML = content;
+        else item.textContent = content;
+        message.list.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+
+    function writeMessage(from, message) {
+        writeToMessageList(`<b>${from}</b>: ${escapeHtml(message)}`, true);
+    }
+
+    function escapeHtml(unsafe) {
+        return unsafe
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
+})();
